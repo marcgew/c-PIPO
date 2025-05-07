5874e34f3ede4fb7c2f08021fd9f39eff6b632fc
marc <marcgeweiler@web.de>
2025-05-07 22:46:36 +0000

    ai init

---
+++ llm/client.py
@@ -0,0 +1,28 @@
+import os
+from groq import Groq
+
+
+GROQ_API_KEY = "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"
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
+
+def send_to_llm(prompt: str) -> str:
+    chat_completion = client.chat.completions.create(
+        messages=[
+             {
+            "role": "system",
+            "content": "Du bist cPipo, ein hilfsbereiter Sprachassistent ähnlich wie C-3PO. Du sprichst höflich, präzise und leicht förmlich."
+            },
+            {   
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+        model="llama-3.3-70b-versatile",
+    )
+
+    message_content = chat_completion.choices[0].message.content
+    return message_content
+
+
--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-from audio import input, output
-from llm-backend import client
+from audio import audio_input, audio_output 
+from llm import client
 from display import face
+
+from llm import client
+
+def main():
+    while True:  
+        user_input = input("Sag etwas: ")
+        if user_input.lower() == 'exit':  
+            print("Beende das Programm...")
+            break
+        reply = client.send_to_llm(user_input)
+        print("🤖 Antwort:", reply)
+
+if __name__ == "__main__":
+    main()
